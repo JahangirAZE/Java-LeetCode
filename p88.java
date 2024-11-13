457f8eac29a7f4ae1e6631b1e523b32d87c0ea1b
Cahangir Əlisoy <156217652+JahangirAZE@users.noreply.github.com>
2024-11-14 03:49:24 +0400

    Update p88.java

--- p88.java
+++ p88.java
@@ -1,7 +1,7 @@
 class Solution {
   public void merge(int[] nums1, int m, int[] nums2, int n)
   {
-    int mIndex = m - 1, nIndex = n - 1, left = m + n - 1;
+    int mIndex = m - 1, nIndex = n - 1, right = m + n - 1;
     while (nIndex >= 0)
     {
       if (mIndex >= 0 && nums1[mIndex] > nums2[nIndex])
