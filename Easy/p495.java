e3ec7aa9ba15799375b468a85aab44811296d3d7
Cahangir Əlisoy <156217652+JahangirAZE@users.noreply.github.com>
2024-12-06 00:19:35 +0400

    Update p495.java

--- Easy/p495.java
+++ Easy/p495.java
@@ -1,15 +1,11 @@
 class Solution {
   public int findPoisonedDuration(int[] timeSeries, int duration) {
-    int total = 0;
-    for (int i = 0; i < timeSeries.length-1; i++) {
-      // if next attack occurs before current duration ends, include the difference
-      if (timeSeries[i+1] <= timeSeries[i] + duration-1) {
-        total += timeSeries[i+1] - timeSeries[i];
-      } else { // add duration normally
-        total += duration;
-      }
+    if (timeSeries.length == 0) return 0;
+    int count = 0;
+    for (int i = 0; i < timeSeries.length - 1; ++i)
+    {
+      count += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
     }
-    total += duration; // include last attack from teemo
-    return total;
+    return count + duration;
   }
 }
