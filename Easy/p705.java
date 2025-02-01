f426d030c1272362a4787e19a139192a2aa838fb
Cahangir Əlisoy <156217652+JahangirAZE@users.noreply.github.com>
2025-02-01 19:45:42 +0400

    Create p705.java

---
+++ Easy/p705.java
@@ -0,0 +1,20 @@
+class MyHashSet {
+  
+  private final boolean[] hashset;
+
+  public MyHashSet() {
+    hashset = new boolean[1000001];
+  }
+
+  public void add(int key) {
+    hashset[key] = true;
+  }
+
+  public void remove(int key) {
+    hashset[key] = false;
+  }
+
+  public boolean contains(int key) {
+    return hashset[key];
+  }
+}
