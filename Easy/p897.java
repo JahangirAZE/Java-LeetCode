abba8484706e9e421719795f5013de0db5a42b80
Cahangir Əlisoy <156217652+JahangirAZE@users.noreply.github.com>
2025-02-11 18:33:32 +0400

    Create p897.java

---
+++ Easy/p897.java
@@ -0,0 +1,13 @@
+class Solution {
+  public TreeNode increasingBST(TreeNode root) {
+    return increasingBST(root, null);
+  }
+
+  public TreeNode increasingBST(TreeNode root, TreeNode tail) {
+    if (root == null) return tail;
+    TreeNode result = increasingBST(root.left, root);
+    root.left = null;
+    root.right = increasingBST(root.right, tail);
+    return result;
+  }
+}
